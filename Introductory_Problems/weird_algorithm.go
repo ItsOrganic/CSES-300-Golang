a99cc489a1b3ea70628c7fde9134c48488824f6c
ItsOrganic <stutyashivam@gmail.com>
2024-12-12 22:59:41 +0530

    first commit

---
+++ Introductory_Problems/weird_algorithm.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+func WeirdAlgo(n int) {
+	fmt.Print(n, " ")
+	if n == 1 {
+		return
+	} else if n%2 == 0 {
+		WeirdAlgo(n / 2)
+	} else {
+		WeirdAlgo(n*3 + 1)
+	}
+}
+
+func MissingNumber(n int, nums []int) int {
+	m := make(map[int]bool)
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] = true
+	}
+	missing := 0
+
+	for i := 1; i <= n; i++ {
+		if !m[i] {
+			missing = i
+		}
+	}
+	return missing
+}
+
+func Repetition(s string) int {
+	i := 0
+	n := len(s)
+	m := make(map[byte]int)
+	mx := 0
+	for j := 0; j < n; j++ {
+		m[s[j]]++
+		if len(m) == 1 {
+			mx = max(mx, m[s[j]])
+		}
+		if len(m) > 1 {
+			for len(m) > 1 {
+				m[s[i]]--
+				if m[s[i]] == 0 {
+					delete(m, s[i])
+				}
+				i++
+			}
+		}
+	}
+	return mx
+}
+
+func IncreasingArray(nums []int) int {
+	count := 0
+	for j := 0; j < len(nums)-1; j++ {
+		if nums[j] > nums[j+1] {
+			count += nums[j] - nums[j+1]
+		}
+	}
+	return count
+}
+
+func main() {
+	WeirdAlgo(3)
+
+	miss := []int{3, 2, 5, 1, 7}
+	fmt.Println()
+	fmt.Println("Missing number = ", MissingNumber(5, miss))
+
+	s := "ATTCGGGA"
+	fmt.Println("Most number of times a word Repeated : ", Repetition(s))
+
+	arr := []int{3, 2, 5, 1, 7}
+	fmt.Println("Increasing array : ", IncreasingArray(arr))
+}
